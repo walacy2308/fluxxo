2a08b3a98156291ddb5c042674469d9227c12b2a
walacy2308 <walacyfreitas85@gmail.com>
2026-04-15 01:45:13 -0300

    teste server limpo

--- src/server.js
+++ src/server.js
@@ -1,20 +1,11 @@
 const express = require("express");
-// const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 
-// const supabase = createClient(
-//   process.env.SUPABASE_URL,
-//   process.env.SUPABASE_KEY
-// );
-
-// ROTA DE TESTE (OBRIGATÓRIA)
+// ROTA DE TESTE
 app.get("/", (req, res) => {
   res.send("API ONLINE 🚀");
 });
-
-// bot.startPolling();
-// bot.on(...)
 
 const PORT = process.env.PORT;
 
